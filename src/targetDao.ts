f6344befbae8f05a350de764a41e75598fdf7efd
jekl <102255715+jekl0x@users.noreply.github.com>
2023-07-23 08:22:40 -0400

    Update targetDao.ts

--- src/targetDao.ts
+++ src/targetDao.ts
@@ -7,14 +7,9 @@ export const TARGET_DAO: {
     CHAIN_ID: ValidNetwork;
   };
 } = {
-  "0xf6538c07324f59b3ba685d86393c65dce9676c70": {
-    ADDRESS: "0xf6538c07324f59b3ba685d86393c65dce9676c70",
-    SAFE_ADDRESS: "0xb64b12c4e68310fc222580dea1c86d202310f343",
-    CHAIN_ID: "0x5",
-  },
-  "0xf844b98df9ccdfbe5d460d0d7bdca232cf9da923": {
-    ADDRESS: "0xf844b98df9ccdfbe5d460d0d7bdca232cf9da923",
-    SAFE_ADDRESS: "0xeb0dc703b854791914f30b5a73dd04d8d22a9aff",
-    CHAIN_ID: "0x1",
+  "0x7551eeb016d0c1c8315611053d30bd90d1a4b09b": {
+    ADDRESS: "0x7551eeb016d0c1c8315611053d30bd90d1a4b09b",
+    SAFE_ADDRESS: "0x6ee647cd1512e50cf2938c273657dee076286ec5",
+    CHAIN_ID: "0xa",
   },
 };
