ea5e68a4feff2c3be007af2958c0d3b39250fc43
jekl <102255715+jekl0x@users.noreply.github.com>
2023-10-13 18:46:56 -0400

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,8 +13,8 @@ const LinkBox = styled.div`
 export const Home = () => {
   return (
     <SingleColumnLayout>
-      <H2>We are Silo Squad</H2>
-      <img src="https://cdn.discordapp.com/attachments/495385622354067468/1162518934407348244/Silo_Logo_02_Ring.png?ex=653c3b20&is=6529c620&hm=71bb8fc3b1e35dff0f476d3c35e7fbc34538ad0bac22cf4ede31780a0f32a492&" />
+      <H2 style={{ marginBottom: "1rem" }}>We are Silo Squad</H2>
+      <img src="https://cdn.discordapp.com/attachments/495385622354067468/1162518934407348244/Silo_Logo_02_Ring.png?ex=653c3b20&is=6529c620&hm=71bb8fc3b1e35dff0f476d3c35e7fbc34538ad0bac22cf4ede31780a0f32a492&" width="250" height="250"/>
       <ParMd style={{ marginBottom: "2.4rem" }}>
         We're building NFT DAO tooling so you can squad up with your community and get shit done.
       </ParMd>
